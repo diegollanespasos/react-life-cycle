49466f5cebddb3c4917b133f905ebd49c875a7a1
diegollanespasos <diego.llanes@theksquaregroup.com>
2021-05-26 19:24:04 -0500

    added cases for shouldComponentUpdate, componentDidUpdate and componentWillUnmount

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Note.css';
 import deleteIcon from '../../assets/delete.webp';
 import { fetchingNotes } from '../../services/notesService';
 
-const Note = ({ id, title, message, date, category, updateNotes }) => {
+class Note extends Component {
 
-  const deleteNote= async () => {
+  deleteNote= async () => {
     try {
-      await fetch(`https://www.diegollanes.ml/api/${id}`, { method: 'DELETE' });
+      await fetch(`https://www.diegollanes.ml/api/${this.props.id}`, { method: 'DELETE' });
       const newNotes = await fetchingNotes();
-      updateNotes(newNotes,category);
+      this.props.updateNotes(newNotes,this.props.category);
     } catch(e){
       console.log(e.message);
     }
   }
 
-  const dateFormatted = () => {
-    let newDate = new Date(date);
+  dateFormatted = () => {
+    let newDate = new Date(this.props.date);
     return (((newDate.getMonth() > 8) ? (newDate.getMonth() + 1) : ('0' + (newDate.getMonth() + 1))) + '/' + ((newDate.getDate() > 9) ? newDate.getDate() : ('0' + newDate.getDate())) + '/' + newDate.getFullYear());
   }
 
-  return (
-    <React.Fragment>
-      <div className='note'>
-        <h3>{title}</h3>
-        <p>{message}</p>
-        <div id='bottom-section'>
-          <button onClick={deleteNote}>
-            <img src={deleteIcon} alt='delete' width='30' height='30'/>
-          </button>
-          <p>{dateFormatted()}</p> 
-        </div>  
-      </div>
-    </React.Fragment>
-  );
+  componentWillUnmount(){
+    console.log(`ComponentWillUnmount`);
+    this.props.addCountDeleted();
+  }
+
+  render(){
+    const { title, message } = this.props;
+
+    return (
+      <React.Fragment>
+        <div className='note'>
+          <h3>{title}</h3>
+          <p>{message}</p>
+          <div id='bottom-section'>
+            <button onClick={this.deleteNote}>
+              <img src={deleteIcon} alt='delete' width='30' height='30'/>
+            </button>
+            <p>{this.dateFormatted()}</p> 
+          </div>  
+        </div>
+      </React.Fragment>
+    );
+  }
 };
 
 export default Note;
